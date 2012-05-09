dad820912bebad9d4007d55f2670418270a439b1
Jason Kim at Miiscan <jkim@riavera.com>
2012-05-09 10:07:44 -0400

    put host domain on config/app.php and make git ignore the file

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,14 @@ define('CONFIG_PATH', rtrim(APP_PATH) . '/config');
 define('LIBRARY_PATH', rtrim(APP_PATH) . '/library');
 define('WEBROOT_PATH', rtrim(APP_PATH) . '/webroot');
 define('FILEUPLOAD_PATH', rtrim(APP_PATH) . '/webroot/files');
-define('WEBROOT_URL', 'http://demo.ctm.local:4567');
-define('FILEUPLOAD_URL', WEBROOT_URL . '/files');
 
 // include config
 require_once(CONFIG_PATH.'/app.php');
 require_once(LIBRARY_PATH.'/DatabaseHandler.php');
 require_once(LIBRARY_PATH.'/phpdocx/classes/AutoLoader.inc');
+
+define('WEBROOT_URL', $config['host']['domain']);
+define('FILEUPLOAD_URL', WEBROOT_URL . '/files');
 
 // following section will be changed to autoloader later on...
 // include application libraries
--- config/app.php
+++ config/app.php
@@ -1,5 +1,10 @@
 <?php
+// hostname
+$config['host']['domain'] = "http://ctd.local";
+
 // database
 $config['database']['dsn'] = "mysql:dbname=ctd_demo;host=127.0.0.1";
 $config['database']['user'] = "root";
 $config['database']['pass'] = "qwe123";
+
+
